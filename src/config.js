aa214ebc1036f5a68f39602eb20bb8f1a6616600
Jeremy and Co <swain.cody@gmail.com>
2019-07-10 16:43:40 +0200

    Firebase config. Remove this from github

---
+++ src/config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+import 'firebase/firestore'; 
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDBUgWI3ijASenayUvGg6koBKcGvWDDCXs",
+  authDomain: "slideplanningapp.firebaseapp.com",
+  databaseURL: "https://slideplanningapp.firebaseio.com",
+  projectId: "slideplanningapp",
+  storageBucket: "",
+  messagingSenderId: "396091221361",
+  appId: "1:396091221361:web:3ffc47c237b3dba6",
+};
+
+let app = firebase.initializeApp(firebaseConfig);
+export const db = app.firestore(); 
