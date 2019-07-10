a95d0617c7358bb7eb14925f87f510f5f73c20f6
Jeremy and Co <swain.cody@gmail.com>
2019-07-10 16:39:14 +0200

    Updated sub container renders on home screen

--- components/EventPane.js
+++ components/EventPane.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react';
 import { Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-export default class ActionButton extends React.Component {
+export default class EventPane extends React.Component {
   constructor(props){
     super(props);
   }
 
   _onPress = () => {
-    color_theme = this.props.action_data['color']
-    this.props.navigation('Contact', {color: color_theme})
+    // color_theme = this.props.action_data['color']
+    // this.props.navigation('Contact', {color: color_theme})
   };
 
 	render(){
-    action_name = this.props.action_data['text'];
-    style_color = {"backgroundColor": this.props.action_data['color']};
+    event_name = this.props.event_data['name'];
+    event_type = this.props.event_data['type'];
+    event_address = this.props.event_data['address'];
+    event_photoId = this.props.event_data['photoId'];
 
     return (
       <TouchableOpacity onPress={this._onPress}>
-        <View style={[styles.main, style_color]}>
-          <Text style={styles.title}>{action_name}</Text>
+        <View style={styles.main}>
+          <Text style={styles.title}>Name: {event_name}</Text>
+          <Text style={styles.title}>Type: {event_type}</Text>
+          <Text style={styles.title}>Address: {event_address}</Text>
+          <Text style={styles.title}>photoId: {event_photoId}</Text>
         </View>
       </TouchableOpacity>
     );
@@ -32,7 +37,7 @@ const styles = StyleSheet.create({
       marginTop: 18,
       height: 400,
       borderRadius: 13,
-      backgroundColor: "#16D8C0",
+      backgroundColor: "white",
       justifyContent: 'center',
       alignItems: 'center',
 
