e0a751164ff0e4a61a900adc4bb8db77344e8041
Jeremy and Co <swain.cody@gmail.com>
2019-07-10 16:42:25 +0200

    Retrieve and render event information from firebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet, FlatList, Button, Alert, AsyncStorage } from 'react-native';
+import { 
+  View, 
+  ScrollView, 
+  StyleSheet, 
+  FlatList, 
+  Button, 
+  Alert, 
+  AsyncStorage 
+} from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-import ActionButton from '../components/ActionButton'
+import EventPane from '../components/EventPane'
 import { Contacts } from 'expo';
-import * as Icon from '@expo/vector-icons'
+import * as Icon from '@expo/vector-icons';
+import { db } from '../src/config.js';
+
+// Retrieve all events in firebase
+let get_all = async () => {
+  let snapshot = await db.collection("events").get();
+  events = [];
+  snapshot.forEach(doc => {
+    db_event = doc.data()
+    event = {
+      'name': db_event['name'],
+      'type': db_event['type'],
+      'address': db_event['address'],
+      'photoId': db_event['photoId'],
+    }
+    events.push(event);
+  });
+  return events;
+}
 
 export default class ActionScreen extends React.Component {
   // Note: To bind a function into the navigation options
   // I put _this=this into the componentDidMount method
 
   static navigationOptions =  ({ navigation }) => ({
-    title: 'LooseRoots',
+    title: 'Slide',
     headerTitleStyle: {
       fontSize: 24,
       fontFamily: 'System',
@@ -40,52 +66,53 @@ export default class ActionScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {actions: []}
-  }
+  };
 
   componentDidMount(){
     _this = this; 
 
-    action_list = [
+    events_list = [
       {"text": "This will be a swipeable event", "color": "#ff4081"},
       {"text": "This will also be a swipeable event", "color": "#40c4ff"},
       {"text": "Surfing in Pacifica", "color": "white"},
       {"text": "Hiking in the Alps", "color": "white"}, 
       {"text": "Riding bikes at the marina", "color": "white"},
-      {"text": "Chowing at Pong's", "color": "white"},]
+      {"text": "Chowing at Pong's", "color": "white"},];
 
-      // {"text": "Interested", "color": "#fff"},
-      // {"text": "Smoke Sesh", "color": "#fff"},
-      // {"text": "Hangout", "color": "#fff"},
-      // {"text": "Go Somewhere", "color": "#fff"}, 
-      // {"text": "Chill", "color": "#fff"},
-      // {"text": "Chow Down", "color": "#fff"},]
     this.setState({
-      actions: action_list,
-    })
-  }
+      events: events_list,
+    });
+  };
 
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
         <View style={styles.filterContainer}>
-          
+          <Button title="test retrieve" onPress={this._updateEventContainers} />
         </View>
         <ScrollView style={styles.scrollSwipeContainer}>
           <FlatList
-            data={this.state.actions}
+            data={this.state.events}
             keyExtractor={(item, index) => index.toString()}
-            renderItem={ ({item}) => <ActionButton action_data={item} navigation={navigate}/>}      
+            renderItem={ ({item}) => <EventPane event_data={item} navigation={navigate}/>}      
           />
         </ScrollView>
       </View>
     );
-  }
+  };
+
+  _updateEventContainers = async () => {
+    let all_events = await get_all();
+    this.setState({
+      events: all_events,
+    })
+  };
 
   _signOut = async () => {
     await AsyncStorage.removeItem('userToken');
     this.props.navigation.navigate('Auth');
-  }
+  };
 }
 
 const styles = StyleSheet.create({
