f0550198c15ce03b3b34081ae873f9f98871c623
Cody Swain <swain.cody@gmail.com>
2019-09-05 23:32:15 -0700

    New screens for new plans and posts

---
+++ screens/NewPlanScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class NewPlanScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ screens/NewPostScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class NewPostScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
